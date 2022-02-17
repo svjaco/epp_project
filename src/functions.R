30231ae2882bbf756a4fe7fa972cce2eb3b0555d
svjaco <s.jacobs@uni-bonn.de>
2022-02-17 21:59:40 +0100

    Added R script "functions.R".

---
+++ src/functions.R
@@ -0,0 +1,8 @@
+# Local polynomial regression with the option for explicit boundary adjustment
+
+# Effective Programming Practices for Economists
+# Prof. von Gaudecker
+# Winter 2021/22, M.Sc. Economics, Bonn University
+# Sven Jacobs
+
+################################################################################
