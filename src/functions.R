bb63bad23b484b9062fb2886e6730c76999d798b
svjaco <s.jacobs@uni-bonn.de>
2022-02-22 16:31:04 +0100

    Implemented the boundary-adjusted NW estimator.

--- src/functions.R
+++ src/functions.R
@@ -72,28 +72,28 @@ LP <- function(x, X, Y, kernel = epanechnikov, bw, degree = 1L) {
     #                             regression function at the evaluation points (vector)
 
     # Error messages
-    if(length(X) != length(Y)) {stop("Length of X and Y has to be equal.")}
-    if(bw <= 0) {stop("Bandwidth has to be strictly positive.")}
-    if(TRUE %in% lapply(kernel_list, all.equal, kernel) == FALSE) {
+    if (length(X) != length(Y)) {stop("Length of X and Y has to be equal.")}
+    if (bw <= 0) {stop("Bandwidth has to be strictly positive.")}
+    if (TRUE %in% lapply(kernel_list, all.equal, kernel) == FALSE) {
         stop("Only supported kernels can be chosen.")
     }
-    if(degree < 0 | typeof(degree) != "integer") {
+    if (degree < 0 | typeof(degree) != "integer") {
         stop("degree has to be of type integer and nonnegative.")
     }
 
     # Warning messages
-    if((degree %% 2) == 0) {warning(strwrap("It is strongly recommended to use an
-                                            odd order as odd order polynomial fits
-                                            outperform even order ones.
-                                            In particular, odd order fits are
-                                            boundary adaptive.",
-                                            prefix = " ", initial = ""))}
+    if ((degree %% 2) == 0) {warning(strwrap("It is strongly recommended to use an
+                                             odd order as odd order polynomial fits
+                                             outperform even order ones.
+                                             In particular, odd order fits are
+                                             boundary adaptive.",
+                                             prefix = " ", initial = ""))}
 
     # General messages
-    if(degree > 1) {message(strwrap("It is recommended to use the lowest (odd) order,
-                                    since the bandwidth is used to control the
-                                    model complexity.",
-                                    prefix = " ", initial = ""))}
+    if (degree > 1) {message(strwrap("It is recommended to use the lowest (odd) order,
+                                     since the bandwidth is used to control the
+                                     model complexity.",
+                                     prefix = " ", initial = ""))}
 
     X_matrix <- matrix(NA, length(X), degree + 1)
 
@@ -118,10 +118,10 @@ LP <- function(x, X, Y, kernel = epanechnikov, bw, degree = 1L) {
         effective_kernels[i, ] <- t(make_basis(degree + 1, 1)) %*% auxiliary_matrix
         estimates[i] <- effective_kernels[i, ] %*% Y
 
-        if(degree >= 1) {
+        if (degree >= 1) {
             slopes[i] <- t(make_basis(degree + 1, 2)) %*% auxiliary_matrix %*% Y
         }
-        if(degree >= 2) {
+        if (degree >= 2) {
             curvatures[i] <- 2 * t(make_basis(degree + 1, 3)) %*% auxiliary_matrix %*% Y
         }
 
@@ -208,3 +208,70 @@ triweight_alt_left <- function(u, rho) {
 # List with all boundary kernels
 boundary_kernel_list <- list(uniform_left, epanechnikov_left, biweight_left, triweight_left,
                              uniform_alt_left, epanechnikov_alt_left, biweight_alt_left, triweight_alt_left)
+
+#####################################################
+#    Boundary-adjusted Nadaraya-Watson estimator    #
+#####################################################
+
+NW_boundary <- function(x, X, Y, bw,
+                        kernel_interior = epanechnikov, kernel_left = epanechnikov_left,
+                        boundary_left, boundary_right) {
+
+    # input: - x: evaluation points (vector)
+    #        - X: data for the regressor (vector)
+    #        - Y: data for the regressand (vector)
+    #        - bw: bandwidth (scalar)
+    #        - kernel_interior: kernel used in the interior (function), with default
+    #        - kernel_left: left boundary kernels (function), with default
+    #        - boundary_left: lower boundary of the support of X (scalar)
+    #        - boundary_right: upper boundary of the support of X (scalar)
+
+    # output: - list containing
+    #               - estimates: estimates for the regression function
+    #                            at the evaluation points (vector)
+    #               - effective_kernels: effective kernels
+    #                                    at the evaluation points (matrix)
+
+    # Error messages
+    if (length(X) != length(Y)) {stop("Length of X and Y has to be equal.")}
+    if (bw <= 0) {stop("Bandwidth has to be strictly positive.")}
+    if (TRUE %in% lapply(kernel_list, all.equal, kernel_interior) == FALSE) {
+        stop("Only supported kernels can be chosen.")
+    }
+    if (TRUE %in% lapply(boundary_kernel_list, all.equal, kernel_left) == FALSE) {
+        stop("Only supported boundary kernels can be chosen.")
+    }
+    if (boundary_left > min(X)) {
+        stop("The lower boundary cannot be larger than the smallest observed value of the regressor X.")
+    }
+    if (boundary_right < max(X)) {
+        stop("The upper boundary cannot be smaller than the largest observed value of the regressor X.")
+    }
+
+    absolute_weights <- matrix(NA, length(x), length(Y))
+
+    for (i in 1:length(x)) {
+
+        if (x[i] >= boundary_left & x[i] < boundary_left + bw) {
+
+            absolute_weights[i, ] <- kernel_left(u = (X - x[i])/bw, rho = (x[i] - boundary_left)/bw)
+
+        } else if (x[i] >= boundary_left + bw & x[i] <= boundary_right - bw) {
+
+            absolute_weights[i, ] <- kernel_interior((X - x[i])/bw)
+
+        } else if (x[i] > boundary_right - bw & x[i] <= boundary_right) {
+
+            absolute_weights[i, ] <- kernel_left(u = -(X - x[i])/bw, rho = (boundary_right - x[i])/bw)
+
+        }
+
+    }
+
+    effective_kernels <- absolute_weights/rowSums(absolute_weights)
+
+    estimates <- as.vector(effective_kernels %*% Y)
+
+    list(estimates = estimates, effective_kernels = effective_kernels)
+
+}
