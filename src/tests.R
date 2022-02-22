1392a42714f863fc04548a368577360be374959c
svjaco <s.jacobs@uni-bonn.de>
2022-02-22 17:05:56 +0100

    Checks for boundary-adjusted NW estimator.

--- src/tests.R
+++ src/tests.R
@@ -17,15 +17,12 @@ set.seed(123) # Seed for reproducibility
 library(locpol) # Kernel local polynomial regression with weights
 
 # We use the package "locpol" (published on CRAN) to validate our
-# implementation of the local polynomial estimator.
+# implementation of the local polynomial estimator and
+# the cross-validated bandwidth selector.
 
 ################################################################################
-#              Tests for LP function (Local polynomial estimator)              #
+#                           Generating example data                            #
 ################################################################################
-
-#################################
-#    Generating example data    #
-#################################
 
 m_fun <- function(x) {sin(2*pi*x)} # True regression function
 n <- 100 # Sample size
@@ -33,48 +30,52 @@ X <- seq(0, 1, length.out = n) # Observed values for regressor
 m_X <- m_fun(X) # True values of regression function
 epsilon <- rnorm(n, sd = 0.25) # Error term
 Y <- m_X + epsilon # Observed values for regressand
-h <- 0.2 # Bandwidth
+bw <- 0.2 # Bandwidth
 df <- data.frame(Y, X) # data frame, required for locpol
+
+################################################################################
+#              Tests for LP function (Local polynomial estimator)              #
+################################################################################
 
 #########################################################
 #    Checks for messages (errors, warnings, general)    #
 #########################################################
 
 # Error messages
-LP_unequal_length <- LP(x = X, X = X[1:(n-1)], Y = Y, bw = h)
-LP_negative_bw <- LP(x = X, X = X, Y = Y, bw = -h)
-LP_non-supported_kernel <- LP(x = X, X = X, Y = Y, kernel = dnorm, bw = h) # dnorm is the Gaussian kernel
-LP_negative_degree <- LP(x = X, X = X, Y = Y, bw = h, degree = -1L)
-LP_non-integer_degree <- LP(x = X, X = X, Y = Y, bw = h, degree = 0.5)
+LP_unequal_length <- LP(x = X, X = X[1:(n-1)], Y = Y, bw = bw)
+LP_negative_bw <- LP(x = X, X = X, Y = Y, bw = -bw)
+LP_non-supported_kernel <- LP(x = X, X = X, Y = Y, kernel = dnorm, bw = bw) # dnorm is the Gaussian kernel
+LP_negative_degree <- LP(x = X, X = X, Y = Y, bw = bw, degree = -1L)
+LP_non-integer_degree <- LP(x = X, X = X, Y = Y, bw = bw, degree = 0.5)
 
 # Warning messages
-LP_even_degree <- LP(x = X, X = X, Y = Y, bw = h, degree = 0L)
+LP_even_degree <- LP(x = X, X = X, Y = Y, bw = bw, degree = 0L)
 
 # General messages
-LP_higher_degree <- LP(x = X, X = X, Y = Y, bw = h, degree = 3L)
+LP_higher_degree <- LP(x = X, X = X, Y = Y, bw = bw, degree = 3L)
 
 ##########################################################
 #    Checks for estimates for the regression function    #
 ##########################################################
 
 # Nadaraya-Watson (degree = 0)
-output_NW_LP <- LP(x = X, X = X, Y = Y, bw = h, degree = 0L)
+output_NW_LP <- LP(x = X, X = X, Y = Y, bw = bw, degree = 0L)
 estimates_NW_LP <- output_NW_LP$estimates
-output_NW_locpol <- locpol(Y ~ X, df, bw = h, deg = 0) # locpol uses the Epanechnikov kernel by default
+output_NW_locpol <- locpol(Y ~ X, df, bw = bw, deg = 0) # locpol uses the Epanechnikov kernel by default
 estimates_NW_locpol <- output_NW_locpol$lpFit$Y
 all.equal(estimates_NW_LP, estimates_NW_locpol) # Should yield TRUE
 
 # Local linear (degree = 1)
-output_LL_LP <- LP(x = X, X = X, Y = Y, bw = h, degree = 1L)
+output_LL_LP <- LP(x = X, X = X, Y = Y, bw = bw, degree = 1L)
 estimates_LL_LP <- output_LL_LP$estimates
-output_LL_locpol <- locpol(Y ~ X, df, bw = h, deg = 1)
+output_LL_locpol <- locpol(Y ~ X, df, bw = bw, deg = 1)
 estimates_LL_locpol <- output_LL_locpol$lpFit$Y
 all.equal(estimates_LL_LP, estimates_LL_locpol) # Should yield TRUE
 
 # Higher degree (degree = 3)
-output_cubic_LP <- LP(x = X, X = X, Y = Y, bw = h, degree = 3L)
+output_cubic_LP <- LP(x = X, X = X, Y = Y, bw = bw, degree = 3L)
 estimates_cubic_LP <- output_cubic_LP$estimates
-output_cubic_locpol <- locpol(Y ~ X, df, bw = h, deg = 3)
+output_cubic_locpol <- locpol(Y ~ X, df, bw = bw, deg = 3)
 estimates_cubic_locpol <- output_cubic_locpol$lpFit$Y
 all.equal(estimates_cubic_LP, estimates_cubic_locpol) # Should yield TRUE
 
@@ -100,3 +101,26 @@ all.equal(slopes_cubic_LP, slopes_cubic_locpol) # Should yield TRUE
 curvatures_cubic_LP <- output_cubic_LP$curvatures
 curvatures_cubic_locpol <- output_cubic_locpol$lpFit$Y2
 all.equal(curvatures_cubic_LP, curvatures_cubic_locpol) # Should yield TRUE
+
+################################################################################
+#        Tests for NW_boundary function (Boundary-adjusted NW estimator)       #
+################################################################################
+
+######################################
+#    Checks for messages (errors)    #
+######################################
+
+NW_boundary_unequal_length <- NW_boundary(x = X, X = X[1:(n-1)], Y = Y, bw = bw,
+                                          boundary_left = 0, boundary_right = 1)
+NW_boundary_negative_bw <- NW_boundary(x = X, X = X, Y = Y, bw = -bw,
+                                       boundary_left = 0, boundary_right = 1)
+NW_boundary_non-supported_kernel <- NW_boundary(x = X, X = X, Y = Y, bw = bw,
+                                                kernel_interior = dnorm,
+                                                boundary_left = 0, boundary_right = 1)
+NW_boundary_non-supported_boundary_kernel <- NW_boundary(x = X, X = X, Y = Y, bw = bw,
+                                                         kernel_left = dnorm,
+                                                         boundary_left = 0, boundary_right = 1)
+NW_boundary_left_boundary_too_large <- NW_boundary(x = X, X = X, Y = Y, bw = bw,
+                                                   boundary_left = 0.5, boundary_right = 1)
+NW_boundary_right_boundary_too_small <- NW_boundary(x = X, X = X, Y = Y, bw = bw,
+                                                    boundary_left = 0, boundary_right = 0.5)
